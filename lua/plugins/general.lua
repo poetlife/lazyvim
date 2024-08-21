27c7df88ba1479fd1d8324dc71816ceea6f1f8d1
jidongpeng <jidongpeng@tencent.com>
2024-08-21 10:05:16 +0800

    feat: set gitsigns show line git blame info

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -16,4 +16,14 @@ return {
       },
     },
   },
+  -- 配置gitsigns
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true, -- git blame当前行
+      current_line_blame_opts = {
+        delay = 100,
+      },
+    },
+  },
 }
