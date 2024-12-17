53d36d6dd1ad23fc8b97ffad17a690818e0c6222
jidongpeng <jidongpeng@tencent.com>
2024-12-17 14:41:09 +0800

    feat: remove telescope
    lazyvim 14.x use fzf-lua as dafault fuzzy finder, so remove telescope
    config

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -1,21 +1,4 @@
 return {
-  -- telescope.nvim 配置
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      {
-        "<leader>ss",
-        function()
-          require("telescope.builtin").lsp_document_symbols({
-            symbols = LazyVim.config.get_kind_filter(),
-            -- 设置 symbols 的宽度为 50
-            symbol_width = 50,
-          })
-        end,
-        desc = "Goto symbols",
-      },
-    },
-  },
   -- 配置gitsigns
   {
     "lewis6991/gitsigns.nvim",
