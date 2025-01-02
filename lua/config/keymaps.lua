49b2c5867d57f8da7a89dea21a54ecb2f266acb8
jidongpeng <jidongpeng@tencent.com>
2025-01-02 12:52:14 +0800

    feat: 终端模式下,按 esc 可以退出

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
 
+-- terminal
+
+-- 在终端模式下，按 Esc 可以进入 Normal 模式
+map("t", "<Esc>", "<C-\\><C-n>", { desc = "change terminal mode to normal mode" })
