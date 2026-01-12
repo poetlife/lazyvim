e255e71fb39f161a7081dc2ba17dea7e68257836
jidongpeng <jidongpeng@tencent.com>
2026-01-12 19:33:34 +0800

    feat: 新版本lazyvim使用nvm-lint替代原有的none-ls

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -3,31 +3,6 @@ return {
     "stevearc/conform.nvim",
     enable = false,
   },
-  -- null-ls
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      {
-        "mason-org/mason.nvim",
-        opts = {
-          ensure_installed = { "golangci-lint" },
-        },
-      },
-    },
-    opts = function()
-      local nls = require("null-ls")
-      return {
-        sources = {
-          -- golangci-lint
-          nls.builtins.diagnostics.golangci_lint,
-          -- goimports
-          nls.builtins.formatting.goimports,
-          -- auto impl interface
-          nls.builtins.code_actions.impl,
-        },
-      }
-    end,
-  },
   {
     "neovim/nvim-lspconfig",
     opts = {
