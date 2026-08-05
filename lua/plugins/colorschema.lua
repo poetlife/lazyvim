a51ca6a9eb39f6574ce30c46e548bbbea8b2783e
pykt <jidongpeng@tencent.com>
2026-08-05 15:59:01 +0800

    fix: use catppuccin theme on Neovim 0.12

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -3,24 +3,23 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    opts = {
+      integrations = {
+        cmp = true,
+      },
+      transparent_background = false,
+      custom_highlights = function(colors)
+        return {
+          Comment = { fg = colors.flamingo },
+          Pmenu = { bg = colors.none },
+        }
+      end,
+    },
   },
-  -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
-      opts = {
-        integrations = {
-          cmp = true,
-        },
-        transparent_background = false,
-        custom_highlights = function(colors)
-          return {
-            Comment = { fg = colors.flamingo },
-            Pmenu = { bg = colors.none },
-          }
-        end,
-      },
+      colorscheme = "catppuccin-nvim",
     },
   },
 }
